f6ef0d29da45fcdf533e602aefdf15e09b005aaa
muhammad.noman <muhammad.noman@bentley.com>
2021-01-17 15:54:17 +0500

    setup
    setup

---
+++ main.js
@@ -0,0 +1,45 @@
+// Modules
+const {app, BrowserWindow} = require('electron')
+var colors = require('colors');
+console.log(colors.yellow('NomanColors'));
+const bcrypt = require('bcrypt');
+bcrypt.hash('myPlaintextPassword', 10, function(err, hash) {
+  console.log(hash);
+  // Store hash in your password DB.
+});
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow
+
+// Create a new BrowserWindow when `app` is ready
+function createWindow () {
+console.log("M.NOMAN");
+  mainWindow = new BrowserWindow({
+    width: 1000, height: 800,
+    webPreferences: { nodeIntegration: true }
+  })
+
+  // Load index.html into the new BrowserWindow
+  mainWindow.loadFile('index.html')
+
+  // Open DevTools - Remove for PRODUCTION!
+  mainWindow.webContents.openDevTools();
+
+  // Listen for window being closed
+  mainWindow.on('closed',  () => {
+    mainWindow = null
+  })
+}
+
+// Electron `app` is ready
+app.on('ready', createWindow)
+
+// Quit when all windows are closed - (Not macOS - Darwin)
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+// When app icon is clicked and app is running, (macOS) recreate the BrowserWindow
+app.on('activate', () => {
+  if (mainWindow === null) createWindow()
+})
